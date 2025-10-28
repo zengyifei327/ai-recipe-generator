92a1a631229be7bed7ee8eedb91ad25517b3c373
Yifei Zeng <yzeng33@my.bcit.ca>
2025-10-28 13:47:53 -0700

    3

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -22,7 +22,7 @@ const bedrockDataSource = backend.data.resources.graphqlApi.addHttpDataSource(
 bedrockDataSource.grantPrincipal.addToPrincipalPolicy(
   new PolicyStatement({
     resources: [
-      "arn:aws:bedrock:us-east-1::foundation-model/anthropic.claude-3-sonnet-20240229-v1:0",
+      "arn:aws:bedrock:us-west-2::foundation-model/anthropic.claude-3-sonnet-20240229-v1:0",
     ],
     actions: ["bedrock:InvokeModel"],
 
